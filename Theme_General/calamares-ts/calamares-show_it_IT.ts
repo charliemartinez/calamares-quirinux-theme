56ac3a362ad9133e7ca7e1b85cd9201db0b3dbc3
Charlie Martinez <39219855+charliemartinez@users.noreply.github.com>
2023-04-03 22:09:33 +0200

    Add files via upload

---
+++ Theme_General/calamares-ts/calamares-show_it_IT.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>show</name>
+    <message>
+        <location filename="show.qml" line="61"/>
+        <source>&lt;h1&gt;Quirinux General&lt;/h1&gt;&lt;h2&gt;Based on Debian GNU/Linux&lt;/h2&gt;&lt;h3&gt;https://www.quirinux.org/&lt;/h3&gt;&lt;p&gt;The UX and UI experience of Quirinux Pro,&lt;/br&gt; the operating system of animated movies,&lt;/br&gt; now also in this edition designed for general users,&lt;/br&gt; which includes everything except programs for animation.&lt;/p&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="show.qml" line="86"/>
+        <source>&lt;h1&gt;Quirinux GNU/Linux&lt;/h1&gt;&lt;h2&gt;The &quot;Chameleon&quot;:&lt;/h2&gt;&lt;h3&gt;The only operating system that adapts to you&lt;/h3&gt;&lt;p&gt;Choose from 12 screen themes including retro or vintage styles.&lt;/p&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="show.qml" line="112"/>
+        <source>&lt;h1&gt;Quirinux GNU/Linux&lt;/h1&gt;&lt;h2&gt;Community support&lt;/h2&gt;&lt;p&gt;You can create a free account at http://foro.quirinux.org&lt;/p&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="show.qml" line="137"/>
+        <source>&lt;h1&gt;Quirinux GNU/Linux&lt;/h1&gt;&lt;h2&gt;Created by: Charlie Martinez&lt;/h2&gt;&lt;h3&gt;email: cmartinez@quirinux.org&lt;/h3&gt;&lt;p&gt;Logo created by: Thomas Gaya&lt;/p&gt;&lt;p&gt;Beta Tester: Leonardo Besingor&lt;/p&gt;&lt;p&gt;ISO creation system Penguin Eggs: Piero Proietti&lt;/p&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
